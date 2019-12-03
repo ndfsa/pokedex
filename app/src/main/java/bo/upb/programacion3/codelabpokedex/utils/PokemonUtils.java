f90a8be4d59aa9abde06fb651766d49332909ef6
ndfsa <25588077+ndfsa@users.noreply.github.com>
2019-12-02 21:40:20 -0400

    pokemons

--- app/src/main/java/bo/upb/programacion3/codelabpokedex/utils/PokemonUtils.java
+++ app/src/main/java/bo/upb/programacion3/codelabpokedex/utils/PokemonUtils.java
@@ -27,6 +27,17 @@ public class PokemonUtils {
         pokemons.add(new Pokemon(10, "Sandshrew", R.drawable.sandshrew, "Ground"));
         pokemons.add(new Pokemon(11, "Venonat", R.drawable.venonat, "Bug"));
         pokemons.add(new Pokemon(12, "Zubat", R.drawable.zubat, "Poison"));
+
+        pokemons.add(new Pokemon(13, "Fearow", R.drawable.fearow, "Flying"));
+        pokemons.add(new Pokemon(14, "Nidoqueen", R.drawable.nidoqueen, "Ground"));
+        pokemons.add(new Pokemon(15, "Vulpix", R.drawable.vulpix, "Fire"));
+        pokemons.add(new Pokemon(16, "Ninetales", R.drawable.ninetales, "Fire"));
+        pokemons.add(new Pokemon(17, "Growlithe", R.drawable.growlithe, "Fire"));
+        pokemons.add(new Pokemon(18, "Poliwrath", R.drawable.poliwrath, "Water"));
+        pokemons.add(new Pokemon(19, "Tentacruel", R.drawable.tentacruel, "Water"));
+        pokemons.add(new Pokemon(20, "Rapidash", R.drawable.rapidash, "Fire"));
+        pokemons.add(new Pokemon(21, "Magneton", R.drawable.magneton, "Steel"));
+        pokemons.add(new Pokemon(22, "Dewgong", R.drawable.dewgong, "Ice"));
         return pokemons;
     }
 }
---
+++ app/src/main/java/bo/upb/programacion3/codelabpokedex/utils/User.java
@@ -0,0 +1,5 @@
+package bo.upb.programacion3.codelabpokedex.utils;
+
+public enum User {
+    Ash, Misty, Brook, Jessy, James
+}
